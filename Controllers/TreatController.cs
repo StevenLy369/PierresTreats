28969fca546b39e8c5219a6196d2374eae55f541
Steven Ly <1stevenly92@gmail.com>
2020-06-05 14:47:32 -0700

    got delete view to work

--- Controllers/TreatController.cs
+++ Controllers/TreatController.cs
@@ -101,23 +101,38 @@ namespace PierresTreat.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
-    [Authorize]
-    [HttpPost]
+       [Authorize]
     public ActionResult Delete(int id)
     {
-      var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       return View(thisTreat);
     }
-
+    [Authorize]
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
-      _db.Treats.Remove(thisTreat);
+      var thisTreats = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreats);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    // [Authorize]
+    // [HttpPost]
+    // public ActionResult Delete(int id)
+    // {
+    //   var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+    //   return View(thisTreat);
+    // }
+
+    // [HttpPost, ActionName("Delete")]
+    // public ActionResult DeleteConfirmed(int id)
+    // {
+    //   var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
+    //   _db.Treats.Remove(thisTreat);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
 
 
   }
