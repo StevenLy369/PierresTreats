1a4efb84fdd34e6d59958a8836fd08dcf08f0a52
Steven Ly <1stevenly92@gmail.com>
2020-06-05 13:17:18 -0700

    only logged in users can edit/create flavors

--- Controllers/FlavorController.cs
+++ Controllers/FlavorController.cs
@@ -1,18 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
-using PierresTreat.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+
+using PierresTreat.Models;
 
 namespace PierresTreat.Controllers
 {
+
   public class FlavorsController : Controller
   {
     private readonly PierresTreatContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public FlavorsController(PierresTreatContext db)
+    public FlavorsController(UserManager<ApplicationUser> userManager,PierresTreatContext db)
     {
       _db = db;
+      _userManager = userManager;
     }
 
     public ActionResult Index()
@@ -20,16 +30,23 @@ namespace PierresTreat.Controllers
       List<Flavor> model = _db.Flavors.ToList();
       return View(model);
     }
-
+    [Authorize]
     public ActionResult Create()
     {
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Flavor flavor)
+    public async Task<ActionResult> Create(Flavor flavor, int TreatId)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      flavor.User = currentUser;
       _db.Flavors.Add(flavor);
+      if (TreatId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
@@ -42,13 +59,13 @@ namespace PierresTreat.Controllers
           .FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
-
+    [Authorize]
     public ActionResult Edit(int id)
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
-
+    [Authorize]
     [HttpPost]
     public ActionResult Edit(Flavor flavor)
     {
@@ -56,13 +73,13 @@ namespace PierresTreat.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
+    [Authorize]
     public ActionResult Delete(int id)
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
       return View(thisFlavor);
     }
-
+    [Authorize]
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
--- Models/Flavor.cs
+++ Models/Flavor.cs
@@ -11,6 +11,7 @@ namespace PierresTreat.Models
 
     public int FlavorId { get; set; }
     public string Word { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<FlavorTreat> Treats { get; set; }
   }
 }
